08064a4a48f33529ea46f6ea99fd6e417b540b10
MohamedArshath10 <arshath.m2003@gmail.com>
2025-03-13 12:14:04 +0530

    Working on Image uploading

--- Frontend/src/Pages/Auth/SignUp.jsx
+++ Frontend/src/Pages/Auth/SignUp.jsx
@@ -3,6 +3,7 @@ import AuthLayout from '../../Components/Layouts/AuthLayout'
 import {Link, useNavigate} from 'react-router-dom'
 import Input from '../../Components/Inputs/Input'
 import { validateEmail } from '../../Utils/helper'
+import ProfilePhotoSelector from '../../Components/Inputs/ProfilePhotoSelector'
 
 const SignUp = () => {
   const[profilePic, setProfilePic] = useState(null)
@@ -23,8 +24,34 @@ const SignUp = () => {
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>Join us today by entering your details below</p>
 
         <form onSubmit={handleSignUp}>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
 
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+            <Input 
+              value={fullName}
+              onChange={({target}) => setFullName(target.value)}
+              label="Full name"
+              placeholder="John"
+              type={Text}
+            />
+
+          <Input 
+            value = {email}
+            onChange = {({target}) => setEmail(target.value)}
+            label = "Email Address"
+            placeholder = "arshath@gmail.com"
+            type = "text"
+          />
+
+          <div className='col-span-2'>
+            <Input 
+              value = {password}
+              onChange = {({target}) => setPassword(target.value)}
+              label = "Password"
+              placeholder = "Min 8 characters"
+              type = "password"
+            />
+          </div>
           </div>
         </form>
       </div>
