fda9b52a16eb8294bd972b37f2501f46d6ab3d8b
MohamedArshath10 <arshath.m2003@gmail.com>
2025-03-16 16:34:55 +0530

    Db is connected and the post method is completed

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {protect} = require('../middleware/authMiddleware')
 
 const {registerUser, loginUser, getUserInfo,} = require("../controller/authController")
 
@@ -6,6 +7,6 @@ const router = express.Router()
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
-// router.post("/getUser", protect, getUserInfo)
+router.get("/getUser", protect, getUserInfo)
 
 module.exports = router
