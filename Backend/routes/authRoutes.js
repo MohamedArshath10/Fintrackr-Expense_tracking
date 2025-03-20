cd1834e8a5cede66c2fbe983236b928f0252b3dd
MohamedArshath10 <arshath.m2003@gmail.com>
2025-03-20 12:25:12 +0530

    Working on income routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {protect} = require('../middleware/authMiddleware')
 
 const {registerUser, loginUser, getUserInfo,} = require("../controller/authController")
+const upload = require('../middleware/uploadMiddleware')
 
 const router = express.Router()
 
@@ -9,6 +10,13 @@ router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/getUser", protect, getUserInfo)
 
-router.post()
+router.post("/upload-image", upload.single("image"), (req, res) => {
+    if(!req.file){
+        return res.status(400).json({ message: "No file uploaded"})
+    }
+    const imageUrl = `${req.protocol}://${req.post("host")}/upload/${req.file.filename}`
+
+    res.status(200).json({imageUrl})
+})
 
 module.exports = router
