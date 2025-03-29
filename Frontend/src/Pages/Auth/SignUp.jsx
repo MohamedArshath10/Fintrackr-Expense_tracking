9107383157a12bd6507685db94ff540d40d56edf
MohamedArshath10 <arshath.m2003@gmail.com>
2025-03-29 13:30:21 +0530

    Working on Sign in api integeration

--- Frontend/src/Pages/Auth/SignUp.jsx
+++ Frontend/src/Pages/Auth/SignUp.jsx
@@ -1,9 +1,13 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import AuthLayout from '../../Components/Layouts/AuthLayout'
 import {Link, useNavigate} from 'react-router-dom'
 import Input from '../../Components/Inputs/Input'
 import { validateEmail } from '../../Utils/helper'
 import ProfilePhotoSelector from '../../Components/Inputs/ProfilePhotoSelector'
+import { API_PATHS } from '../../Utils/apiPaths'
+import axiosInstance from '../../Utils/axiosInstance'
+import { UserContext } from '../../Context/useContext'
+import uploadImage from '../../Utils/uploadImage'
 
 const SignUp = () => {
   const[profilePic, setProfilePic] = useState(null)
@@ -11,6 +15,8 @@ const SignUp = () => {
   const[email, setEmail] = useState("")
   const[password, setPassword] = useState("")
   const[error, setError] = useState(null)
+
+  const {updateUser} = useContext(UserContext)
 
   const navigate = useNavigate()
 
@@ -38,7 +44,35 @@ const SignUp = () => {
     setError("");
 
     // Sign Up api call
+    try{
 
+      // Upload profile image if present
+      if(profilePic){
+        const imgUploadRes = await uploadImage(profilePic)
+        profileImageUrl = imgUploadRes.imageUrl || ""
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password
+      })
+
+      const {token, user} = response.data
+
+      if(token){
+        localStorage.setItem("token", token)
+        updateUser(user)
+        navigate("/dashboard")
+      }
+    }catch(error){
+      if(error.response && error.response.data.message){
+        setError(error.response.data.message)
+      }
+      else{
+        setError("Something went wrong")
+      }
+    }
   }
 
   return (
