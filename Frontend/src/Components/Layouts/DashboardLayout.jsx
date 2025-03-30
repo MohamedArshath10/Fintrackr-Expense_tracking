fb678bb159362a8ca1e1ccfeabfaa5062fe32485
MohamedArshath10 <arshath.m2003@gmail.com>
2025-03-30 16:10:30 +0530

    Working on Dashbosrd

---
+++ Frontend/src/Components/Layouts/DashboardLayout.jsx
@@ -0,0 +1,26 @@
+import React, { Children, act, useContext } from 'react'
+import { UserContext } from '../../Context/useContext'
+import Navbar from './Navbar'
+import SideMenu from './SideMenu'
+
+const DashboardLayout = ({children, activeMenu}) => {
+    const {user} = useContext(UserContext)
+  return (
+    <div className=''>
+        <Navbar activeMenu={activeMenu}/>
+
+        {user && (
+            <div className='flex'>
+                <div className=''>
+                    <SideMenu activeMenu={activeMenu} />
+                </div>
+                <div className='grow mx-5'>
+                    {children}
+                </div>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default DashboardLayout
